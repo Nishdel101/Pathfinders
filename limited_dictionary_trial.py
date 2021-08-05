39f99f32e2bef9a7c6b8b97c780df5667229348b
Nishanth Delavictoire <67369744+Nishdel101@users.noreply.github.com>
2021-08-06 01:09:00 +0200

    Recognizes certain commands ready for rosnode

--- limited_dictionary_trial.py
+++ limited_dictionary_trial.py
@@ -3,10 +3,15 @@ from pocketsphinx import LiveSpeech, get_model_path
 
 model_path = get_model_path()
 
+global wordList
+
+
+wordsList=['yes','no','yeah','nope']
+
 speech = LiveSpeech(
     verbose=False,
-    sampling_rate=32000,
-    buffer_size=4096,
+    sampling_rate=8000,
+    buffer_size=256,
     no_search=False,
     full_utt=False,
     hmm=os.path.join(model_path, 'en-us'),
@@ -14,5 +19,9 @@ speech = LiveSpeech(
     dic=os.path.join(model_path, 'cmudict-en-us.dict')
 )
 
-for phrase in speech:
-    print(phrase)
+for speechPhrase in speech:
+
+    phrase=str(speechPhrase)
+    for word in phrase:
+        print("here")
+        #insert ros node code here
