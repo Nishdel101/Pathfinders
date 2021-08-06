e1c8511462d7bdf14e4eaff555fc4c86d0c81610
Nishanth Delavictoire <67369744+Nishdel101@users.noreply.github.com>
2021-08-06 02:15:10 +0200

    Program detects words(tested) + ros pub(not tested)
    logic is written within the file itself. ros pub not yet tested.

--- limited_dictionary_trial.py
+++ limited_dictionary_trial.py
@@ -1,12 +1,44 @@
+#! Python 3.9.6
+"""
+logic-
+code scans speech, converts it to a string, and checks if the string contains one of the words we require
+it then maps this word accordingly to a number specified in the word dictionary(as it takes less data ad hence higher probability of success i think , to send an integer via ros pub.)
+this integer is passed to the pub class and sent to the bot to tell the bot if for example it has to look for another person or whatever.
+
+'BEFORE CODING FURTHER'
+line 60 is debug and line 61 is the actual ros pub method call so when trying to publish , comment and uncomment the apt lines
+
+
+"""
+
 import os
 from pocketsphinx import LiveSpeech, get_model_path
-
+"""
+import rospy
+from std_mdsgs.msg import Int16
+"""
 model_path = get_model_path()
-
 global wordList
+wordsList=['yes','no','yeah','nope']        #vocabulary to be increased
 
+wordListdict = {
+    'yes':1,
+    'no':2,
+    'yeah':3,
+    'nope':4
+}
 
-wordsList=['yes','no','yeah','nope']
+"""
+def voicePublisher(pubInt):
+    pub = rospy.Publisher('Speech', Int16, queue_size=10) #defined as Int as it is less data to send than string
+    rospy.init_node('VoiceRecognition', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        publishedInteger = pubInt % rospy.get_time()
+        rospy.loginfo(PublishedInteger)
+        pub.publish(PublishedInteger)
+        rate.sleep()
+"""
 
 speech = LiveSpeech(
     verbose=False,
@@ -19,9 +51,11 @@ speech = LiveSpeech(
     dic=os.path.join(model_path, 'cmudict-en-us.dict')
 )
 
-for speechPhrase in speech:
 
+for speechPhrase in speech:
+    #print(speechPhrase,'1')    #Debugging statment
     phrase=str(speechPhrase)
-    for word in phrase:
-        print("here")
-        #insert ros node code here
+    #print(phrase,'2')          #Debugging statement
+    if phrase in wordsList: #meant to screen out words here itself as it would be faster than sending each speech message over ros publisher(and also potentially losing speechmessages)
+        print(wordListdict[phrase])          #Debugging statement
+        #voicePublisher(wordListdict[phrase])
