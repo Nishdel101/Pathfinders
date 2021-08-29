412edea93e2317de31392b37b4f7f37c11aa6dca
Nishanth Delavictoire <67369744+Nishdel101@users.noreply.github.com>
2021-08-29 17:54:25 +0200

    updated

--- image_processing/scripts/moving_image.py
+++ image_processing/scripts/moving_image.py
@@ -30,6 +30,7 @@ def Area(length,breadth):
 def Body_detect(msg):
     global body_detect_flag
     body_detect_flag=1
+    print("Body_detect")
     for detection in msg.bounding_boxes:
         if detection.probability > 0.5:
             length=detection.xmax - detection.xmin
@@ -39,24 +40,31 @@ def Body_detect(msg):
     x_max_body=msg.bounding_boxes[i_body].xmax
     x_min_body=msg.bounding_boxes[i_body].xmin
     centre_body=(x_max_body + x_min_body)/2  
+    print("Body_detect SUCCESSFUL")
     
 def Face_detect(msg):
+    print("Face_Detect")
     global face_detect_flag
     face_detect_flag=1
     for detection in msg.faces:
         area_Faces.append(Area(detection.width,detection.height))
     i_face= area_Faces.index(max(area_Faces))              #assuming that person with biggest body would also show the biggest face
     centre_face= msg.faces[i_face].face.x
+    print("Face_detect_SUCCESSFUL")
     
 def Movement(): 
+    print("Movement")
     if centre_face >= centre_body+80 and centre_face<=centre_body+80 :
         movement = center_body-(image_width/2)
         if movement > 80 or movement < -80 :
             print(f'move {movement} pixels')
-
+    print("Movement SUCCESSFUL")
+    
 rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, Body_detect)
 rospy.Subscriber('/face_detection/faces', FaceArrayStamped, Face_detect)
+print("FINISHED CALLBACKS")
 if body_detect_flag==1 and face_detect_flag==1:
+    print("ENTERING MOVEMENT")
     Movement()
     body_detect_flag=0
     face_detect_flag=0
