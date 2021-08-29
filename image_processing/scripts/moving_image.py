3442ee5a155d5b1d7d63193a02cb08174641f88d
Nishanth Delavictoire <67369744+Nishdel101@users.noreply.github.com>
2021-08-29 15:19:59 +0200

    updated

--- image_processing/scripts/moving_image.py
+++ image_processing/scripts/moving_image.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 from darknet_ros_msgs.msg import BoundingBoxes
+from opencv_appsmsg import FaceArrayStamped
 
 rospy.init_node('moving_image')
 
@@ -14,6 +15,10 @@ global centre_body
 global centre_face
 global body_detect_flag
 global face_detect_flag
+global area_Faces
+global area_Body
+area_Faces=[]
+area_Body=[]
 centre_body = []
 centre_face = []
 
@@ -23,23 +28,25 @@ def Area(length,breadth):
 
 
 def Body_detect(msg):
+    global body_detect_flag
     body_detect_flag=1
     for detection in msg.bounding_boxes:
         if detection.probability > 0.5:
             length=detection.xmax - detection.xmin
             breadth=detection.ymax- detection.ymin
             area_Body.append(Area(length,breadth))
-    i_body = area_Body.index(max(area))
+    i_body = area_Body.index(max(area_Body))
     x_max_body=msg.bounding_boxes[i].xmax
     x_min_body=msg.bounding_boxes[i].xmin
     centre_body=(x_max_body + x_min_body)/2  
     
 def Face_detect(msg):
+    global face_detect_flag
     face_detect_flag=1
     for detection in msg.faces:
-        area_Face.append(Area(detection.width,detection.height))
-    i_face= area_faces.index(max(area))              #assuming that person with biggest body would also show the biggest face
-    centre_face= msg.faces[i_face].face.x)
+        area_Faces.append(Area(detection.width,detection.height))
+    i_face= area_Faces.index(max(area_Faces))              #assuming that person with biggest body would also show the biggest face
+    centre_face= msg.faces[i_face].face.x
     
 def Movement(): 
     if centre_face >= centre_body+80 and centre_face<=centre_body+80 :
@@ -48,7 +55,7 @@ def Movement():
             print(f'move {movement} pixels')
 
 rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, Body_detect)
-rospy.Subscriber('/open_CV/bounding_boxes', Faces, Face_detect)
+rospy.Subscriber('/face_detection/faces', FaceArrayStamped, Face_detect)
 if body_detect_flag==1 and face_detect_flag==1:
     Movement()
     body_detect_flag=0
