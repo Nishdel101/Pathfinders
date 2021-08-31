1c7cb279c0ead2e2b71c93bc9438118cf1cfed1a
Raj Sakhrekar <68809828+rajsak97@users.noreply.github.com>
2021-08-31 19:56:43 +0200

    Update single_publisher.py

--- image_processing/scripts/single_publisher.py
+++ image_processing/scripts/single_publisher.py
@@ -1,29 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 from std_msgs.msg import String
+from opencv_apps.msg import FaceArrayStamped
 
 global temp
 temp="0"
 
 
 def callback(data):
-
+    #print("checkingtype",type(data))
     global temp
     #rospy.loginfo(rospy.get_caller_id() + " middle man %s", data.data)
-    print("before",temp)
-    if temp!=data.data:
-        temp=data.data
+    #print("before",temp)
+    if temp!=data.faces:
+        #print("data.faces",data.faces)
+        temp=data.faces
         #print("maybe ill publish now")
         #print("after",temp)
-        pub.publish(temp)
+        pub.publish(data)  #weird fix but temp changes to a list because of data.faces and has to be converted back because of the publish function
 
 
 def intermediate():
     global pub
+    print("running")
     rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber("image", String, callback)
-    pub = rospy.Publisher('instruction', String, queue_size=10)
+    rospy.Subscriber("face_detection/faces", FaceArrayStamped, callback)
+    pub = rospy.Publisher('instruction', FaceArrayStamped, queue_size=10)
     rospy.spin()
 
 if __name__ == '__main__':
